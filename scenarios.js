347f4640ca78c4d1dca36fb1a03a8eaed647e417
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-15 19:55:05 +0100

    Add answer to episode 8

--- scenarios.js
+++ scenarios.js
@@ -140,3 +140,41 @@ const verdict = declareMurderer();
 console.log(verdict);
 
 // output: The murderer is Mr. Green.
+
+
+const scenario = {
+    murderer: 'Mrs. Peacock',
+    room: 'Conservatory',
+    weapon: 'Lead Pipe'
+  };
+  
+  const changeScenario = function() {
+    scenario.murderer = 'Mrs. Peacock';
+    scenario.room = 'Dining Room';
+  
+    const plotTwist = function(room) {
+      if (scenario.room === room) {
+        scenario.murderer = 'Colonel Mustard';
+      }
+  
+      const unexpectedOutcome = function(murderer) {
+        if (scenario.murderer === murderer) {
+          scenario.weapon = 'Candle Stick';
+        }
+      }
+  
+      unexpectedOutcome('Colonel Mustard');
+    }
+  
+    plotTwist('Dining Room');
+  }
+  
+  const declareWeapon = function() {
+    return `The weapon is ${scenario.weapon}.`
+  }
+  
+  changeScenario();
+  const verdict = declareWeapon();
+  console.log(verdict);
+
+//   output: The weapon is Candle Stick.
