4f0b600ca4dd329b527187e36948aa388e2d9274
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-15 18:46:39 +0100

    Add answer to episode 2

--- scenarios.js
+++ scenarios.js
@@ -12,3 +12,20 @@ const scenario = {
   console.log(verdict);
 
 //   Output: The murderer is Miss Scarlet.
+
+
+const murderer = 'Professor Plum';
+
+const changeMurderer = function() {
+  murderer = 'Mrs. Peacock';
+}
+
+const declareMurderer = function() {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//   Output: Error => we can't reassign const variables.
