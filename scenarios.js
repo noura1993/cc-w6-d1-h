4a6394547bcb63d5f538ed57fb52012213dcee43
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-15 19:12:53 +0100

    Add answer to episode 6

--- scenarios.js
+++ scenarios.js
@@ -87,3 +87,26 @@ const scenario = {
   console.log(verdict);
 
 //   output: The weapon is the Revolver.
+
+
+let murderer = 'Colonel Mustard';
+
+const changeMurderer = function() {
+  murderer = 'Mr. Green';
+
+  const plotTwist = function() {
+    murderer = 'Mrs. White';
+  }
+
+  plotTwist();
+}
+
+const declareMurderer = function () {
+  return `The murderer is ${murderer}.`;
+}
+
+changeMurderer();
+const verdict = declareMurderer();
+console.log(verdict);
+
+//   output: The murderer is Mrs. White.
