5d5350c9cf63946bd928c1f42e8b3733875d7fce
Nourhan Elhakim <nourhanmagdy993@gmail.com>
2020-06-15 18:41:43 +0100

    Add answer to episode 1

---
+++ scenarios.js
@@ -0,0 +1,14 @@
+const scenario = {
+    murderer: 'Miss Scarlet',
+    room: 'Library',
+    weapon: 'Rope'
+  };
+  
+  const declareMurderer = function() {
+    return `The murderer is ${scenario.murderer}.`;
+  }
+  
+  const verdict = declareMurderer();
+  console.log(verdict);
+
+//   Output: The murderer is Miss Scarlet.
